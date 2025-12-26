e3aa48ef01dfa581b66738423739690341f7fa91
Wessel_Weernink <wesselweernink@gmail.com>
2025-12-27 00:00:50 +0100

    Adds auto_install option for treesitter and changes the shortcut for to 'go to definition'.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -32,7 +32,7 @@ return {
         callback = function(ev)
           local opts = { buffer = ev.buf}
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
           vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts) 
         end,
       })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
   lazy = false,
   build = ":TSUpdate",
   opts = {
+    auto_install = {
+      enable = true,
+    }, 
     ensure_installed = {"lua", "javascript", "rust", "c"},
     highlight = {
       enable = true,
