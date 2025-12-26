3811bd09c65e9c77813bdccce7c4a021cec58036
Wessel_Weernink <wesselweernink@gmail.com>
2025-12-26 16:07:07 +0100

    Adds fixes for neo-tree so that you can open a git status and close the window correctly. Adds comments.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -8,8 +8,13 @@ return {
   },
   lazy = false,
   config = function()
-    vim.keymap.set('n', '<C-i>', ':Neotree filesystem reveal left reveal=true<CR>', {})
-    vim.keymap.set('n', '<C-o>', ':Neotree toggle=true', {})
-    vim.keymap.set('n', '<C-p>', ':Neotree float git_status git_base=main', {})
+    -- These command work but beware that <C-o> the ":Neotree filesystem close left<CR>" doesn't work well when neovim is running within
+    -- Mintty on Windows, it won't redraw the screen correctly and have not found a good workaround. 
+    -- This is apparently a known issue with Mintty on Windows, the rest of the commands work fine. My recommendation is 
+    -- to not use the second command while on windows or to use a different terminal other than Mintty. Won't create a different config for 
+    -- this specific Windows case as I want one config for all, just beware and comment that command on windows. 
+    vim.keymap.set('n', '<C-i>', ':Neotree filesystem reveal left<CR>', { desc = 'Reveals Neo-tree filesystem on the left'})
+    vim.keymap.set('n', '<C-o>', ':Neotree filesystem close left<CR>', { desc = 'Closes the neo-tree filesystem'})
+    vim.keymap.set('n', '<C-p>', ':Neotree float git_status<CR>', { desc = 'Shows git status relative to most recent commit at HEAD'})
   end
 }
