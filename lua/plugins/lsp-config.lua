d04fedd0cad3774898bd6d18bfc57615a62d84a2
Wessel_Weernink <wesselweernink@gmail.com>
2025-12-26 18:48:17 +0100

    Adds updated syntax for mason-lspconfig so that it doesn't call require(lspconfig) which is deprecated. Also adds usage of LspAttach to make sure the code actions shortcuts only work when the lsp loads and attaches to the buffer

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -24,13 +24,18 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function() 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("rust_analyzer", {})
 
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(ev)
+          local opts = { buffer = ev.buf}
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts) 
+        end,
+      })
+    end,
   },
 }
