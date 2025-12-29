5b9a92ccf91f15d3c82a44ca5a6418860816e726
Wessel_Weernink <wesselweernink@gmail.com>
2025-12-29 19:17:51 +0100

    Adds linters and formatters for JS/TS, bash & lua. Also makes Mason-lspconfig ensure the right linters and formatters are installed.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,14 +7,23 @@ return {
                 package_installed = "✓",
                 package_pending = "➜",
                 package_uninstalled = "✗"
-            }
-        }
+            },
+        },
     }
   },
   {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = {"lua_ls", "rust_analyzer", "stylua"},
+      ensure_installed = {
+        "lua_ls", -- Lua LSP
+        "rust_analyzer", -- Rust LSP
+        "stylua", -- Lua Formatter
+        "prettier", -- JS/TS Formatter
+        "shfmt", -- Bash Formatter
+        "eslint_d", -- Lua Linter
+        "shellcheck", -- Bash Linter
+        "luacheck" -- Lua Linter
+      },
     },
     dependencies = {
       { "mason-org/mason.nvim", opts = {} },
@@ -23,7 +32,7 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    config = function() 
+    config = function()
 
       vim.lsp.config("lua_ls", {})
       vim.lsp.config("rust_analyzer", {})
@@ -33,7 +42,7 @@ return {
           local opts = { buffer = ev.buf}
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts) 
+          vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, opts)
         end,
       })
     end,
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,10 +5,25 @@ return {
 
     null_ls.setup({
       sources = {
-        null_ls.builtins.formatting.stylua,
-        -- TODO: Need more sources and pre-install through mason-lspconfig.
+        null_ls.builtins.formatting.stylua, -- Lua formatter
+        null_ls.builtins.formatting.prettier, -- JS/TS formatter & others like JSON files.
+        null_ls.builtins.formatting.shfmt, -- Bash formatter
+        null_ls.builtins.diagnostics.eslint_d, -- JS/TS linter
+        null_ls.builtins.diagnostics.luacheck, -- Lua linter
+        null_ls.builtins.diagnostics.shellcheck, -- Bash Linter
+        -- Note: Only certain languages are suitable to install through Mason 
+        -- and none-ls. Rust for example doesn't work as you just add tools like
+        -- rustfmt & clippy through "rustup". You CAN however install all the LSP's.
+        -- Just don't install linters and formatters for languages like rust where the 
+        -- language itself ships linters and formatters for you. 
+        -- Languages that work with null-ls well:
+        -- Javascript / Typescript 
+        -- lua 
+        -- Bash
+        -- null-ls is good for integratting standalone binaries which are linters or 
+        -- formatters.
       },
     })
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file"})
   end
 }
